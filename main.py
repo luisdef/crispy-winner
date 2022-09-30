98c93f57f733fea4bb525c5ce21afd550f88109d
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2022-09-29 23:01:32 -0300

    Add reply option

--- main.py
+++ main.py
@@ -14,12 +14,20 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(TOKEN, TOKEN_SECRET)
 api = tweepy.API(auth)
 
-tweet = input("Twitter message: ")
-image_path = input("Image path (skip if you don't want to post an image): ")
+opt = int(input("0 - tweet\n1 - reply\n> "))
 
-if image_path != "" and numeric(image_path):
-    api.update_status_with_media(status=tweet, filename=image_path)
-else:
-    api.update_status(status=tweet)
+if opt == 0:
+    tweet = input("Twitter message: ")
+    image_path = input("Image path (skip if you don't want to post an image): ")
+    if image_path != "":
+        api.update_status_with_media(status=tweet, filename=image_path)
+    else:
+        api.update_status(status=tweet)
+
+elif opt == 1:
+    tweet_id = input("Tweet id = ")
+    reply = input("Twitter message: ")
+    api.update_status(status=reply, in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
+
 
 print("Message sent.")
